ae9ae50b5f71e12fbe4c663bef5613d285ec56bc
Lyubomir Georgiev <lgeorgiev@securitybulgaria.com>
2022-09-16 18:00:05 +0300

    Initial port to 15.0

--- __manifest__.py
+++ __manifest__.py
@@ -17,6 +17,7 @@
     # for the full list
     'category': 'Administration',
     'version': '0.1',
+    'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
     'depends': ['base_geolocalize'],
